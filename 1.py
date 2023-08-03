e863ee1cfa9a120076e7786f8459211c8e42900a
hackoverflo <141296064+hackoverflo@users.noreply.github.com>
2023-08-03 20:43:53 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import*
 
 lastx, lasty = 0, 0
 
@@ -21,10 +21,15 @@ window.geometry("800x600")
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 
-canvas = tk.Canvas(window, width = '2', height = '2')
 
+
+canvas = tk.Canvas(window, width = '2', height = '2')
+canvas.pack()
 canvas.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 canvas.bind("<Button-1>", xy)
 canvas.bind("<B1-Motion>", addLine)
+
+btn = Button(window, text = 'Delete', bg = 'black', foreground = 'white', command = lambda:canvas.delete('all'))
+btn.grid(column=3, row = 4)
 
 window.mainloop()
