42786f02e1d14e5cb53bd9e8997cab2b668f2ae4
hackoverflo <141296064+hackoverflo@users.noreply.github.com>
2023-08-04 00:38:43 +0530

    Add files via upload

--- gamified.py
+++ gamified.py
@@ -6,23 +6,33 @@ pygame.display.set_caption('image')
 bg = pygame.image.load("edu\doggggg.png").convert()
 quiz = pygame.image.load('edu\dbutton_quiz.png')
 visuals = pygame.image.load('edu\dbutton_visuals.png')
+idea_box = pygame.image.load('edu\dbutton_idea-box.png')
+jokes = pygame.image.load('edu\dbutton_jokes.png')
+facts = pygame.image.load('edu\dbutton_facts.png')
+games = pygame.image.load('edu\dbutton_games.png')
+station = pygame.image.load('edu\square.png')
 spaceship = pygame.image.load('edu\spaceship (1).png')
 spaceshipX = 100
 spaceshipY = 100
 def isCollision(spaceshipX,spaceshipY,x,z):
 	distance = math.sqrt(math.pow(spaceshipX-x,2)+math.pow(spaceshipY-z,2))
-	if distance < 20:
+	if distance <= 20:
 		return True
 	else:
 		return False
+
 
 def spaceShip():
 	screen.blit(spaceship,(spaceshipX,spaceshipY))
 
 def buttons():
-	screen.blit(quiz,(600,600))
-	screen.blit(visuals,(300,300))
-
+	screen.blit(quiz,(600,100))
+	screen.blit(visuals,(700,500))
+	screen.blit(idea_box,(150,150))
+	screen.blit(jokes,(100,250))
+	screen.blit(facts,(300,600))
+	screen.blit(games,(100,500))
+	screen.blit(station,(500,333))
 def quizF():
 	print('passed')
 def visualsF():
@@ -46,20 +56,31 @@ while (status):
 		elif i.type == pygame.KEYUP:
 			if i.key == pygame.K_LEFT or i.key == pygame.K_RIGHT:
 				spaceshipXc = 0
-			elif spaceshipX > 1000:
-				spaceshipX = 1000
-			elif spaceshipX < 0:
-				spaceshipX = 0
-			elif spaceshipY > 666:
-				spaceshipY = 666
-			elif spaceshipY < 0:
-				spaceshipY = 0
+
 			elif i.key == pygame.K_UP or i.key == pygame.K_DOWN:
 				spaceshipYc = 0
-		elif isCollision(spaceshipX,spaceshipY,600,600):
-			quizF()
-		elif isCollision(spaceshipX,spaceshipY,300,300):
-			visualsF()
+		else:
+			pass
+	
+	if spaceshipX >= 1000:
+		spaceshipX = 1000
+	elif spaceshipX <= 0:
+		spaceshipX = 0
+	elif spaceshipY >=666:
+		spaceshipY = 666
+	elif spaceshipY <= 0:
+		spaceshipY = 0
+	spaceShip()
+	if isCollision(spaceshipX,spaceshipY,600,100):
+		spaceshipX = 500
+		spaceshipY = 333
+		spaceShip()
+		quizF()
+	if isCollision(spaceshipX,spaceshipY,700,500):
+		spaceshipX = 500
+		spaceshipY = 333
+		spaceShip()
+		visualsF()
 	spaceshipX += spaceshipXc
 	spaceshipY += spaceshipYc
 	screen.blit(bg,(0,0))
